aa7f4b7e4c9161345517daef1d1ac022cafd4b44
Simon Vargas <c-simon.vargas@hulu.com>
2022-10-31 21:27:16 -0500

    scrapping and calculating currency convertion for Moneymax

--- agentexchange/exchange_maxmoney.go
+++ agentexchange/exchange_maxmoney.go
@@ -1,12 +1,71 @@
 package interactions
 
+import (
+	scraping "currency-exchange-medellin/scraping"
+	"currency-exchange-medellin/utils"
+	"fmt"
+	"strings"
+
+	"github.com/gocolly/colly"
+)
+
 type ExchangeMaxmoney struct {
 	RequestExchange
 }
 
+const (
+	tableTarget          = "div.tab-content table tbody"
+	rowTarget            = "tr"
+	descriptionColTarget = "td:nth-child(1)"
+	purchaseColTarget    = "td:nth-child(2)"
+	onSaleColTarget      = "td:nth-child(3)"
+)
+
 func (reqExchange *ExchangeMaxmoney) selectExchange() ResultExchange {
-	return ResultExchange{
-		Exchange{"USD", 4135.43, "purshace"},
-		0.0,
-	}
+	fmt.Println("----------- Moneymax Currency Exchange ----------------")
+
+	fmt.Printf("Request Currency: %s - Value: %f - OperationType: %s \n",
+		reqExchange.Exchange.CurrencyCode, reqExchange.Exchange.Value, reqExchange.Exchange.OperationType)
+
+	var scraper = scraping.BuildCollyScrapper(scraping.DefaultTimeOutColly)
+
+	currenciesMoneymax := make([]Currency, 0)
+
+	scrapCurrenciesMoneymax(scraper, &currenciesMoneymax)
+
+	scraper.Visit(reqExchange.Url)
+
+	var resultExchange = CalculateConversion(currenciesMoneymax, &reqExchange.Exchange)
+
+	fmt.Printf("Result Exchange Unicambios:\nCurrency: %s\nOperation Type: %s\nValue Operation: %f\nValue Convertion: %f\n",
+		resultExchange.Exchange.CurrencyCode, resultExchange.OperationType, resultExchange.Value, resultExchange.ValueConvertion)
+
+	return resultExchange
+}
+
+func scrapCurrenciesMoneymax(scraper *colly.Collector, currenciesMoneymax *[]Currency) {
+
+	scraper.OnHTML(tableTarget, func(tableHtml *colly.HTMLElement) {
+
+		fmt.Println("Table: ", tableTarget)
+
+		tableHtml.ForEach(rowTarget, func(i int, row *colly.HTMLElement) {
+
+			description := row.ChildText(descriptionColTarget)
+			purchaseValueStr := strings.Replace(row.ChildText(purchaseColTarget), ".", "", -1)
+			onSaleValueStr := strings.Replace(row.ChildText(onSaleColTarget), ".", "", -1)
+			valueToBuy, _ := utils.FromStringToFloat(strings.Trim(purchaseValueStr, "$ "))
+			valueOnSale, _ := utils.FromStringToFloat(strings.Trim(onSaleValueStr, "$ "))
+
+			currencyMoneymax := Currency{
+				description: description,
+				valueToBuy:  valueToBuy,
+				valueOnSale: valueOnSale,
+			}
+
+			fmt.Println("Currency scraped: ", currencyMoneymax)
+
+			*currenciesMoneymax = append(*currenciesMoneymax, currencyMoneymax)
+		})
+	})
 }
